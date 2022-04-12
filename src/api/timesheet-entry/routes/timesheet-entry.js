9c7b105270eef8df854547f6bc991b1be6a25372
Yushaliniepillay <yushalinie97@hotmail.com>
2022-04-12 11:26:35 +0800

    add a new file

---
+++ src/api/timesheet-entry/routes/timesheet-entry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * timesheet-entry router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::timesheet-entry.timesheet-entry');
---
+++ src/api/timesheet-entry/services/timesheet-entry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * timesheet-entry service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::timesheet-entry.timesheet-entry');
